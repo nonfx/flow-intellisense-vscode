065fdb88e56f7700c812d3190c8997487f99fdde
vikas-cldcvr <vikas@cldcvr.com>
2022-08-05 14:09:24 +0530

    FLOW-329 element meta info updated

--- src/config/elements/index.ts
+++ src/config/elements/index.ts
@@ -1,5 +1,9 @@
 import FButton from "./f-button.json";
+import FIconButton from "./f-icon-button.json";
+import FIcon from "./f-icon.json";
 
 export default {
   ...FButton,
+  ...FIconButton,
+  ...FIcon,
 };
