765e4241fe1942ae05291750ae55e99928985330
vikas-cldcvr <vikas@cldcvr.com>
2022-08-12 12:51:15 +0530

    FLOW-362 Update  ,  as per latest documentation.

--- src/config/elements/index.ts
+++ src/config/elements/index.ts
@@ -2,10 +2,14 @@ import FButton from "./f-button.json";
 import FIconButton from "./f-icon-button.json";
 import FIcon from "./f-icon.json";
 import FDiv from "./f-div.json";
+import FText from "./f-text.json";
+import FSpacer from "./f-spacer.json";
 
 export default {
   ...FButton,
   ...FIconButton,
   ...FIcon,
   ...FDiv,
+  ...FText,
+  ...FSpacer,
 };
